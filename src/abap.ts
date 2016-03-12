c2dfb1f756c02e796a38e9d530d377bb30f6c6c3
larshp <larshp@hotmail.com>
2016-03-12 13:12:18 +0000

    add web test

--- src/abap.ts
+++ src/abap.ts
@@ -2,10 +2,27 @@
 
 import * as CodeMirror from 'codemirror';
 
+const COMMENT = "comment";
+const STRING = "string";
+const NUMBER = "number";
+const KEYWORD = "keyword";
+const OPERATOR = "operator";
+const TEXT = "text";
+
 class AbapMode implements CodeMirror.Mode<any> {
 
     public token(stream: CodeMirror.StringStream, state: any) {
-        return "text";
+
+        while (stream.eatSpace()) {}
+
+        if (stream.match(/^\*/)) {
+            stream.skipToEnd();
+            return COMMENT;
+        }
+
+        stream.skipToEnd();
+
+        return TEXT;
     };
 
 }

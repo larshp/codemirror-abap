c1ce8fbf05b92c879520c7fdb10359fd8fb83ad6
larshp <larshp@hotmail.com>
2016-05-08 12:50:55 +0000

    hacks

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,5 @@
 
-(function(mod) {
+/*(function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../node_modules/codemirror/lib/codemirror"));
   else if (typeof define == "function" && define.amd) // AMD
@@ -7,5 +7,6 @@
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
+*/
     CodeMirror.defineMode("abap", ABAPFactory);
-});
+//});

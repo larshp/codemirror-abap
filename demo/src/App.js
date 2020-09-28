0e71fe0c453c938accac09be62c37600e78f6896
Laurens Deprost <ldeprost@gmail.com>
2020-09-28 20:43:54 +0200

    feat: Initial demo functionality

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.scss';
 import { UnControlled as CodeMirrorControl } from 'react-codemirror2';
-import { abapMode, initAbapMode } from 'codemirror-mode-abap/lib/index';
 import CodeMirror from 'codemirror';
 import 'codemirror/addon/display/fullscreen';
+import 'codemirror-abap';
 
 function App() {
-  initAbapMode(CodeMirror);
   return (
     <div className="App">
       <CodeMirrorControl
-        value={`* codemirror-mode-abap: playground
+        value={`* codemirror-abap: playground
 WRITE 'Hello world!'.`}
         options={{
           theme: 'material-darker',
